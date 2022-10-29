b1a202f120fb848e332e0fd97f47c605b39e218f
Emanuel <jose.eosousa@gmail.com>
2022-10-28 23:11:22 -0300

    create the ChatBox layout

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ChatContext } from '../Context/ChatProvider';
 
-const ChatBox = () => {
-  return <div>ChatBox</div>;
+import { Box } from '@chakra-ui/react';
+import SingleChat from './SingleChat';
+
+const ChatBox = ({ fetchAgain, setFetchAgain }) => {
+  const { selectedChat } = useContext(ChatContext);
+
+  return (
+    <Box
+      display={{ base: selectedChat ? 'flex' : 'none', md: 'flex' }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      w={{ base: '100%', md: '68%' }}
+      borderRadius="lg"
+      bgColor="#23272A"
+      boxShadow="4px 4px 5px 0px rgba(35, 39, 42, 0.75)"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+    </Box>
+  );
 };
 
 export default ChatBox;
