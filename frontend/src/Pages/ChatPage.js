7bf43c6bbe8e5303a82d5fd9f7bfac06f05f7418
Emanuel <jose.eosousa@gmail.com>
2022-10-28 23:19:06 -0300

    added FetchAgain logic and changed ChatContext

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
-import { ChatState } from '../Context/ChatProvider';
-
+import { ChatContext } from '../Context/ChatProvider';
 import { Box } from '@chakra-ui/react';
 import SideDrawer from '../components/miscellaneous/SideDrawer';
 import MyChats from '../components/MyChats';
 import ChatBox from '../components/ChatBox';
 
 const ChatPage = () => {
+  const ChatState = () => {
+    return useContext(ChatContext);
+  };
+
   const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
 
   return (
     <div style={{ width: '100%' }}>
@@ -20,8 +24,10 @@ const ChatPage = () => {
         h="91.5vh"
         p="10px"
       >
-        {user && <MyChats />}
-        {user && <ChatBox />}
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
       </Box>
     </div>
   );
