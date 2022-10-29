42b91b68029323452e38a4887ea22be35f49e334
Emanuel <jose.eosousa@gmail.com>
2022-10-28 23:11:03 -0300

    change the ChatProvider logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,40 @@
-import { Route } from "react-router-dom";
-import "./App.css";
-import ChatPage from "./Pages/ChatPage";
-import HomePage from "./Pages/HomePage";
+import { useEffect, useState } from 'react';
+import { ChatContext } from './Context/ChatProvider';
+import { Route, useHistory } from 'react-router-dom';
+import './App.css';
+import ChatPage from './Pages/ChatPage';
+import HomePage from './Pages/HomePage';
 
 function App() {
+  const [user, setUser] = useState();
+  const [selectedChat, setSelectedChat] = useState();
+  const [chats, setChats] = useState();
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    setUser(userInfo);
+
+    if (!userInfo) history.push('/');
+  }, [history]);
+
   return (
-    <div className="App">
-      <Route path="/" component={HomePage} exact />
-      <Route path="/chats" component={ChatPage} />
-    </div>
+    <ChatContext.Provider
+      value={{
+        user,
+        setUser,
+        selectedChat,
+        setSelectedChat,
+        chats,
+        setChats,
+      }}
+    >
+      <div className="App">
+        <Route path="/" component={HomePage} exact />
+        <Route path="/chats" component={ChatPage} />
+      </div>
+    </ChatContext.Provider>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import ChatProvider from './Context/ChatProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
-    <ChatProvider>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </ChatProvider>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </BrowserRouter>
 );
